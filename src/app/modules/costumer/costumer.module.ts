9c1eb49d7cee86b769f5a5b15b717d8ae8b448d3
lucas <lucas.desouzaca730@gmail.com>
2024-10-29 15:15:18 -0300

    Organization Pages and Components

--- src/app/modules/costumer/costumer.module.ts
+++ src/app/modules/costumer/costumer.module.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { CostumerRoutingModule } from './costumer-routing.module';
 import { MainComponent } from './pages/main/main.component';
-import { SideMenuComponent } from './pages/main/components/side-menu/side-menu.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 
 
