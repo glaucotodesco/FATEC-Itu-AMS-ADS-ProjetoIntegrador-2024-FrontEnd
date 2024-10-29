947c042b392e5da8880e7f99b055fd594ec637a8
Yuuya Okatani <yuuyaokatani@gmail.com>
2024-10-29 14:43:18 -0300

    refactor

--- src/app/modules/admin/components/card-prod-dropdown/card-prod-dropdown.component.ts
+++ src/app/modules/admin/components/card-prod-dropdown/card-prod-dropdown.component.ts
@@ -10,23 +10,23 @@ export class CardProdDropdownComponent {
     {
       id: 1, class: 'Bebidas', percentange: 60,
       products: [
-        { id: 1, name: 'Coca-cola', price: 10, percentage: 50 },
-        { id: 2, name: 'Pepsi', price: 10, percentage: 10  },
-        { id: 3, name: 'Sprite', price: 10, percentage: 10 },
-        { id: 4, name: 'Fanta', price: 10, percentage: 13 },
-        { id: 5, name: 'Guaraná', price: 10, percentage: 17 },
+        { id: 1, name: 'Coca-cola 500ml', price: 5, percentage: 50.5 },
+        { id: 2, name: 'Pepsi 500ml', price: 4.20, percentage: 9 },
+        { id: 3, name: 'Sprite 500ml', price: 5.20, percentage: 10.5 },
+        { id: 4, name: 'Fanta 350ml', price: 3, percentage: 12.3},
+        { id: 5, name: 'Guaraná 500ml', price: 4, percentage: 17.7 },
       ]
     },
     {
       id: 2, class: 'Pratos', percentange: 30,
       products: [
-        { id: 1, name: 'Macarronada', price: 10, percentage: 20 },
-        { id: 2, name: 'Pizza', price: 10, percentage: 10 },
-        { id: 3, name: 'Hamburger', price: 10, percentage: 10 },
-        { id: 4, name: 'Arroz com feijão', price: 30, percentage: 30 },
-        { id: 5, name: 'Esfiha', price: 10, percentage: 20 },
+        { id: 1, name: 'Macarronada', price: 10, percentage: 19.5 },
+        { id: 2, name: 'Pizza', price: 50, percentage: 10.5 },
+        { id: 3, name: 'Hamburger', price: 10, percentage: 9.5 },
+        { id: 4, name: 'Arroz com feijão', price: 10, percentage: 29.5 },
+        { id: 5, name: 'Esfiha', price: 10, percentage: 20.9 },
 
-        { id: 6, name: 'Lasanha', price: 10, percentage: 10 }
+        { id: 6, name: 'Lasanha', price: 20, percentage: 10.1}
 
 
 
@@ -35,12 +35,12 @@ export class CardProdDropdownComponent {
     },
     {
       id: 3, class: 'Sobremesas', percentange: 10, products: [
-        { id: 1, name: 'Brownie', price: 10, percentage: 9 },
-        { id: 2, name: 'Bolo de cereja', price: 10 , percentage: 11 },
-        { id: 3, name: 'Torta de chocolate', price: 10, percentage: 12 },
-        { id: 4, name: 'Cookies', price: 10, percentage: 13 },
-        { id: 5, name: 'Pavlova', price: 10, percentage: 20  },
-        { id: 6, name: 'Mousse de chocolate', price: 10, percentage: 35 },
+        { id: 1, name: 'Brownie', price: 3, percentage: 7.5 },
+        { id: 2, name: 'Bolo de cereja (1 fatia)', price: 7 , percentage: 12.5 },
+        { id: 3, name: 'Torta de chocolate (1 fatia)', price: 10, percentage: 12.3 },
+        { id: 4, name: 'Cookie', price: 3, percentage: 12.5 },
+        { id: 5, name: 'Pavlova', price: 10, percentage: 19.2  },
+        { id: 6, name: 'Mousse de chocolate', price: 9, percentage: 35 },
 
       ]
     }
