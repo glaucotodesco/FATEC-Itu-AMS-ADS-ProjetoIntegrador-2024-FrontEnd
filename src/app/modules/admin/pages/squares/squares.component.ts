14ddc7afa6dd6ea8b8d2d0bbcf5243b04b9ba97d
Maria <maric.brito15@gmail.com>
2024-11-01 21:17:55 -0300

    :sparkles: feat: add exemples employees

--- src/app/modules/admin/pages/squares/squares.component.ts
+++ src/app/modules/admin/pages/squares/squares.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrl: './squares.component.css'
 })
 export class SquaresComponent {
+  examplewaiter = [
+    {
+      name: "Romulo",
+      status: 1,
+      block: false,
+    },
+    {
+      name: "Iara",
+      status: 2,
+      block: false,
+    },
+    {
+      name: "Diogenes",
+      status: 3,
+      block: true,
+    },
+  ]
+
+  exampleCheckout = [
+    {
+      name: "Leandra",
+      status: 1,
+      block: false,
+    },
+    {
+      name: "Julio",
+      status: 2,
+      block: false,
+    },
+  ]
 
 }
