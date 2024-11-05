933dd721e6d574c30a2e7ac5c607b069e89d4cda
AngeloModanez <angelomodanez30@gmail.com>
2024-11-05 16:38:23 -0300

    :sparkles: feat: Login Navigation

--- src/app/modules/waiter/waiter-routing.module.ts
+++ src/app/modules/waiter/waiter-routing.module.ts
@@ -1,11 +1,14 @@
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { OrderDetailsComponent } from './pages/order-details/order-details.component';
 import { MenuComponent } from './pages/menu/menu.component';
 import { OrderTicketsComponent } from './pages/order-tickets/order-tickets.component';
+import { LoginWaiterComponent } from './pages/login-waiter/login-waiter.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginWaiterComponent },
   { path: '', component: HomeComponent },
   { path: 'order', component: OrderDetailsComponent },
   { path: 'menu', component: MenuComponent },
