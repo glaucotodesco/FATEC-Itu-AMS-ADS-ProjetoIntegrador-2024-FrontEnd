4286cee90cd4a24cb111427a6e6e941ca5901aa5
Pedro Vinincius <viniciuspedro0905@gmail.com>
2024-11-17 17:46:49 -0300

    :sparkles: feat: Added modals in squares

--- src/app/modules/admin/components/square-table/square-table.component.ts
+++ src/app/modules/admin/components/square-table/square-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-square-table',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrl: './square-table.component.css'
 })
 export class SquareTableComponent {
+  @Output()
+  editSquareEmitter = new EventEmitter();
 
+  @Output()
+  addEmitter = new EventEmitter();
+
+  @Output()
+  editEmitter = new EventEmitter();
+
+  @Output()
+  removeEmitter = new EventEmitter();
+
+  editSquare(){
+    this.editSquareEmitter.emit();
+  }
+
+  add(){
+    this.addEmitter.emit();
+  }
+
+  edit(){
+    this.editEmitter.emit();
+  }
+
+  remove(){
+    this.removeEmitter.emit();
+  }
 }
--- src/app/modules/admin/pages/squares/squares.component.ts
+++ src/app/modules/admin/pages/squares/squares.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from '../../../../shared/components/modal/modal.component';
 
 @Component({
   selector: 'app-squares',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './squares.component.css'
 })
 export class SquaresComponent {
-
+  openModal(modalForm: ModalComponent, params : any = {}){
+    modalForm.open(params);
+  }
 }
