0d2fb00a058edcd9b5dfd129cf371533f20100af
Yuuya Okatani <yuuyaokaatani@gmail.com>
2024-11-22 18:50:27 -0300

    refactor images

--- src/app/modules/admin/components/about-us-upper/about-us-upper.component.ts
+++ src/app/modules/admin/components/about-us-upper/about-us-upper.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-about-us-upper',
@@ -6,8 +6,56 @@ import { Component } from '@angular/core';
   styleUrl: './about-us-upper.component.css'
 })
 export class AboutUsUpperComponent {
-  items = [
-    1, 2, 3, 4,
-  ]
+
+  selectedImage: string | null = null;
+  selectedImage2: string | null = null;
+
+  @ViewChild('fileInput')
+  fileInput!: ElementRef<HTMLInputElement>;
+
+  @ViewChild('fileInput2')
+  fileInput2!: ElementRef<HTMLInputElement>;
+
+  triggerFileInput(){
+    console.log('sos');
+    this.fileInput.nativeElement.click();
+    
+  }
+
+  triggerFileInput2(){
+    console.log('sos');
+    this.fileInput2.nativeElement.click();
+    
+  }
+
+  items: any = []
+  
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.selectedImage = reader.result as string;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  onFileSelected2(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.selectedImage2 = reader.result as string;
+        this.items.push(this.selectedImage2)
+        console.log(this.items);
+        
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+
+
 
 }
