ded0c1d08402e22aed0c95de58563c09af078ea1
muriisantuss@gmail.com <muriisantuss@gmail.com>
2024-12-03 14:40:12 -0300

    feat: criando os services do projeto

---
+++ src/app/services/categories.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Categories } from '../interfaces/categories';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+  private url = "http://localhost:8080/categories";
+
+  constructor(private http: HttpClient) {}
+
+  getCategories(): Observable<Categories[]> {
+    return this.http.get<Categories[]>(this.url);
+  }
+  getCategoriesById(id: number): Observable<Categories> {
+    return this.http.get<Categories>(`${this.url}/${id}`);
+  }
+  postCategories(categories: Categories): Observable<Categories> {
+    return this.http.post<Categories>(this.url, categories);
+  }
+  putCategories(categories: Categories): Observable<Categories> {
+    return this.http.put<Categories>(`${this.url}/${categories.id}`, categories);
+  }
+  deleteCategories(categories: Categories): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${categories.id}`);
+  }
+}
